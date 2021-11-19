9bf0b2eb8c3bf6f9c5d6f776225ff8058bda7fd4
Diego <dfreile51@gmail.com>
2021-11-19 13:30:18 +0100

    02 Corregido

--- Telefono.java
+++ Telefono.java
@@ -1,34 +1,67 @@
 public class Telefono {
     
     // El dueño del teléfono
-    private String dueño;
+    private String dueno;
     // Cantidad de la memoria del teléfono
     private int cantidadDeMemoria;
     // Encendido o apagado del teléfono
     private boolean telefonoEncendido;
     
     
-    public Telefono(String nombreDueño, int cantidadMemoria, boolean encendidoApagado) {
-        dueño = nombreDueño;
+    public Telefono(String nombreDueno, int cantidadMemoria) {
+        dueno = nombreDueno;
         cantidadDeMemoria = cantidadMemoria;
-        telefonoEncendido = encendidoApagado;
+        telefonoEncendido = false;
     }
     
-    public int aumentoMemoria(int cantidadDeAumento) {
+    public void aumentoMemoria(int cantidadDeAumento) {
         cantidadDeMemoria = cantidadDeMemoria + cantidadDeAumento;
-        return cantidadDeMemoria;
     }
     
-    public boolean apretarBotonEncendido(boolean apretarBoton) {
-        telefonoEncendido = apretarBoton;
-        return telefonoEncendido;
+    public void apretarBotonEncendido() {
+        if (telefonoEncendido == false) {
+            telefonoEncendido = true;
+        }
+        else {
+            telefonoEncendido = false;
+        }
+    }
+    
+    public void setDueno(String nuevoDueno) {
+        dueno = nuevoDueno;
     }
     
     public void imprimirInformacion () {
-        System.out.println("Dueño: "+ dueño + " | Cantidad de memoria: " + cantidadDeMemoria + " | Encendido: " + telefonoEncendido);
+        if (telefonoEncendido == true) {
+            System.out.println("Dueño: "+ dueno + " | Cantidad de memoria: " + cantidadDeMemoria + " | Encendido: si");
+        }
+        else {
+           System.out.println("Dueño: "+ dueno + " | Cantidad de memoria: " + cantidadDeMemoria + " | Encendido: no"); 
+        }
+    }
+    
+    public String getEstado() {
+        String estado;
+        String encendido;
+        if (telefonoEncendido == true) {
+            encendido = "si";
+        }
+        else {
+           encendido = "no";
+        }
+        estado = ("Dueño: "+ dueno + " | Cantidad de memoria: " + cantidadDeMemoria + " | Encendido: " + encendido);
+        return estado;
     }
     
     public boolean getEncendido() {
         return telefonoEncendido;
+    }
+     
+    public int getCantidadMemoria() {
+        return cantidadDeMemoria;
+    }
+    
+    public String getDueno() {
+        return dueno;
     }
 }
